c36e0ccbcb98d54ee1fa9f1ce0c77fd984248933
Quang Mai <thienquang2804@gmail.com>
2024-12-09 14:22:36 +0700

    feat: add cutlass plugin for better delete/cut experience

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "LazyFile", "VeryLazy" },
-  opts = {
-    ensure_installed = {
-      "go",
-      "svelte",
+    "nvim-treesitter/nvim-treesitter",
+    event = { "LazyFile", "VeryLazy" },
+    opts = {
+        ensure_installed = {
+            "go",
+            "svelte",
+        },
     },
-  },
 }
