19bd4b0674c748a8d550fddbcd1eb6a56a3efde8
Quang Mai <thienquang2804@gmail.com>
2025-01-02 08:08:14 +0700

    add rust tree-sitter

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,6 +6,11 @@ return {
     opts = {
         animate = { enabeld = false },
         scroll = { enabled = false },
+        styles = {
+            notification = {
+                wo = { wrap = true },
+            },
+        },
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
